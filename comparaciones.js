2d7201aba6590a82a101104215dc4111214db8c5
Lizeth Victoria Franco <lizvictoria754@gmail.com>
2019-01-11 17:57:35 -0500

    Comparaciones

---
+++ comparaciones.js
@@ -0,0 +1,16 @@
+var x = 4, y = '4'
+
+var objeto = {
+  nombre: 'Liz'
+}
+var objeto2 = {
+
+  nombre:'Lizo'
+}
+
+//Comparando dos objetos
+
+objeto === objeto2 //false, Porque se estan comparando el nombre de las variables
+
+objeto.nombre === objeto2.nombre //True, porque se estan comparando el valor del atributo del objeto y es igual, si se cambiara el valor del atributo nombre entonces la comparacion daria false
+
