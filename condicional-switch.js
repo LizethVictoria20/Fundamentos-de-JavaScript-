ff7350ca52ecf704eb361fcff4e412f23ebabe66
Lizeth Victoria Franco <lizvictoria754@gmail.com>
2019-01-12 12:45:46 -0500

    Condicional switch

---
+++ condicional-switch.js
@@ -0,0 +1,32 @@
+let signo = prompt('¿Cual es tu signo?')
+
+switch(signo){
+  case 'geminis':
+  case 'Geminis':
+  console.log('Marcha todo bien, te mejoran de categoría en el trabajo, los negocios dan buenos rendimientos y llueven los halagos a tu inteligencia, comprensión y ternura; muchas felicitaciones, así le das paz a tu espíritu.')
+  break
+  case 'cancer':
+  case 'Cancer':
+  console.log('Es posible que hoy te inclines por el matrimonio, pues estás romántico y crees que es el momento para decidirte. Ojalá no te equivoques de época; y si no te sientes bien seguro, mejor espera un poco para que se aclaren algunos aspectos.')
+  break
+  case 'leo':
+  case 'Leo':
+  console.log('Durante este comienzo de año es muy importante para ti que compartas tus conocimientos y enseñes a otros pacientemente. Compartir el conocimiento te llena de vibraciones positivas para tu vida.')
+  break
+  case 'virgo':
+  case 'Virgo':
+  console.log('Con los tuyos, busca un lugar bien agradable en donde puedas permanecer a tus anchas, sin ninguna preocupación y disfrutando al máximo del ambiente. Es necesario que descanses a pesar de la vida social tan activa que tienes ahora, porque la salud se puede ver comprometida.')
+  break
+  default:
+  console.log('No se encuentra el signo')
+}
+
+
+
+
+
+
+
+let a = 100
+let b = 012
+let c = 002
