465d9ad1236fcf24b14d53f0fc943dca68cfa70d
Lizeth Victoria Franco <lizvictoria754@gmail.com>
2019-03-18 10:29:40 -0500

    Prototype

--- clases.js
+++ clases.js
@@ -3,9 +3,21 @@ function Persona(name, last, altura) {
   this.last = last; //this hace referencia a una clase
   this.altura = altura;
 }
-Persona.prototype.saludar = function() {
+Persona.prototype.saludar = () => {
   console.log(`Mi name is ${this.name} ${this.last}`);
 };
 
-let miName = new Persona("Liz", "Franco", 15);
+Persona.prototype.soyAlto = () => {
+  if (this.altura <= 1.8) {
+    console.log("No eres alto");
+  } else {
+    console.log("Eres alto");
+  }
+};
+
+let miName = new Persona("Liz", "Franco", 1.5);
+let otherName = new Persona("Gordis", "Franco", 1.9);
 miName.saludar();
+miName.soyAlto();
+otherName.saludar();
+otherName.soyAlto();
