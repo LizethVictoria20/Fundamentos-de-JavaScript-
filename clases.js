e82f376b67001a65f90dd03d2ab4b3e5159d60b6
Lizeth Victoria Franco <lizvictoria754@gmail.com>
2019-03-18 11:49:08 -0500

    Pisando una prototype con otro

--- clases.js
+++ clases.js
@@ -1,17 +1,35 @@
+function heredaDe(prototipoHijo, prototipoPadre) {
+  let fn = function() {
+    fn.prototype = prototipoPadre.prototype;
+    prototipoHijo.prototype = new fn();
+    claseHija.prototype.contructor = prototipoHijo;
+  };
+}
+//En JS no hay herencia solo existen los prototipos
+
 function Persona(name, last, altura) {
   this.name = name;
   this.last = last; //this hace referencia a una clase
   this.altura = altura;
 }
-Persona.prototype.saludar = () => {
+Persona.prototype.saludar = function() {
   console.log(`Mi name is ${this.name} ${this.last}`);
 };
 
-Persona.prototype.soyAlto = () => this.altura > 1.8;
+Persona.prototype.soyAlto = function() {
+  this.altura > 1.8;
+};
 
-let miName = new Persona("Liz", "Franco", 1.5);
-let otherName = new Persona("Gordis", "Franco", 1.9);
-miName.saludar();
-miName.soyAlto();
-otherName.saludar();
-otherName.soyAlto();
+function Desarrollador(name, last) {
+  //Herencia de prototype en clases
+  this.name = name;
+  this.last = last;
+}
+heredaDe(Desarrollador, Persona);
+
+Desarrollador.prototype.saludar = function() {
+  console.log(`My name is ${this.name} ${this.last} y soy desarrolladora`);
+};
+
+//let miName = new Persona("Liz", "Franco", 1.5);
+//let otherName = new Persona("Gordis", "Franco", 1.9);
