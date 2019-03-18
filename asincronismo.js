8bef60e9ab5e0b9e95d15a45bbb9d80f85edf2fc
Lizeth Victoria Franco <lizvictoria754@gmail.com>
2019-03-18 15:03:43 -0500

    Asincronismo

--- asincronismo.js
+++ asincronismo.js
@@ -1,7 +1,12 @@
-console.log("a");
-setTimeout(() => console.log("b"), 0);
+<script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"
+/>;
+setTimeout(() => console.log("b"), 2000);
+for (let i = 0; i < 1000000000; i++) {}
+
 function set() {
-  console.log("Me ejecute");
+  console.log("Terminè");
 }
 set();
-console.log("c");
