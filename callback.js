e8097b8646f9d5dcd80b230be3f8dc2656f42d48
Lizeth Victoria Franco <lizvictoria754@gmail.com>
2019-03-19 12:51:28 -0500

    Haciendo multiples requests

--- callback.js
+++ callback.js
@@ -3,10 +3,15 @@
 const API_URL = "https://swapi.co/api/";
 const PEOPLE_URL = "people/:id";
 
-const lukeURL = `${API_URL}${PEOPLE_URL.replace(":id", 1)}`;
 const opts = { crossDomain: true };
 
 const onPeopleResponse = function(person) {
-  console.log(`Hola, soy ${person.name}`);
+  console.log(`Hola, my name is ${person.name}`);
 };
-$.get(lukeURL, opts, onPeopleResponse);
+
+const obtenerPersonaje = function(id) {
+  const url = `${API_URL}${PEOPLE_URL.replace(":id", id)}`;
+  $.get(url, opts, onPeopleResponse);
+};
+
+obtenerPersonaje(2);
